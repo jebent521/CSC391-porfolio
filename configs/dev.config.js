11e987412dbf085987316691a16643ece2aba169
Jonah Ebent <jonahaaronebent@gmail.com>
2026-02-10 10:46:12 -0500

    dev and prod configurations

---
+++ configs/dev.config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  flameAndFlightUrl: "CSC391-Adopt-a-Dragon",
+  fusAltarGuildUrl: "CSC391-Altar-Server-Application"
+};
---
+++ configs/prod.config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  flameAndFlightUrl: "HTML",
+  fusAltarGuildUrl: "/srv/csc391web/team3/minCSS"
+};
