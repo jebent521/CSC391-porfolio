1d7798f05579ccac43f9e7568e35bd0f09f1f4b7
Jonah Ebent <jonahaaronebent@gmail.com>
2026-03-05 10:50:33 -0500

    added submodules

--- configs/prod.config.js
+++ configs/prod.config.js
@@ -1,4 +1,12 @@
 window.APP_CONFIG = {
-  flameAndFlightUrl: "HTML",
-  fusAltarGuildUrl: "/srv/csc391web/team3/minCSS"
+  T01_minHTML: "/srv/csc391web/team3/minHTML",
+  T02_HTML: "/srv/csc391web/team3/HTML",
+  T03_minCSS: "/srv/csc391web/team3/minCSS",
+  T04_CSS: "/srv/csc391web/team3/CSS",
+  T05_minBoot: "/srv/csc391web/team3/minBoot",
+  // T05_Boot: "/srv/csc391web/team3/minCSS",
+  TeamProject: "/srv/csc391web/team3/project",
+  A01_individualHTML: "HTML",
+  A03_individualPortfolio: "#",
+  // A05_testPlan: "testPlan",
 };
